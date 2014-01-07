40213ed6056595c36b611f485ad17cc2f1c21f43
clovis.gakam <clovis.gakam@gmail.com>
2014-01-07 17:10:11 +0100

    improved document

--- adph/src/main/java/org/adorsys/adph/jpa/ROLENAMES.java
+++
@@ -1,6 +0,0 @@
-package org.adorsys.adph.jpa;
-
-public enum ROLENAMES
-{
-   ADMIN
-}
--- adph/src/main/java/org/adorsys/adph/jpa/add.java
+++
@@ -1,5 +0,0 @@
-package org.adorsys.adph.jpa;
-
-import org.adorsys.javaext.description.Description;
-@Description("org.adorsys.adph.jpa.add.description")
-enum add-enum-class-description --title Document State --, , ONGOINGSUSPENDEDtext The state of a documents.
