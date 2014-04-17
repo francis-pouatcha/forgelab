a035a09e7c94acf7789311eddffc07e0531f4a53
gakam clovis <clovis.gakam@gmail.com>
2014-04-17 19:44:57 +0100

    .

--- adpharma/adpharma.client/src/main/java/org/adorsys/adpharma/client/jpa/articlelot/ArticleLotListView.java
+++ adpharma/adpharma.client/src/main/java/org/adorsys/adpharma/client/jpa/articlelot/ArticleLotListView.java
@@ -78,7 +78,7 @@ public class ArticleLotListView
 		viewBuilder.addStringColumn(dataList, "mainPic", "ArticleLot_mainPic_description.title", resourceBundle);
 		viewBuilder.addStringColumn(dataList, "internalPic", "ArticleLot_internalPic_description.title", resourceBundle);
 		viewBuilder.addStringColumn(dataList, "agency", "ArticleLot_agency_description.title", resourceBundle);
-		ViewBuilderUtils.newStringColumn(dataList, "articleName", "Article_articleName_description.title", resourceBundle,200d);
+		ViewBuilderUtils.newStringColumn(dataList, "article.pic", "Article_article_description.title", resourceBundle,200d);
 		viewBuilder.addDateColumn(dataList, "expirationDate", "ArticleLot_expirationDate_description.title", resourceBundle, "dd-MM-yyyy", locale);
 		viewBuilder.addBigDecimalColumn(dataList, "stockQuantity", "ArticleLot_stockQuantity_description.title", resourceBundle, NumberType.INTEGER, locale);
 		viewBuilder.addBigDecimalColumn(dataList, "salesPricePU", "ArticleLot_salesPricePU_description.title", resourceBundle, NumberType.CURRENCY, locale);
--- adpharma/adpharma.server/src/main/java/org/adorsys/adpharma/server/rest/ProcurementOrderEJB.java
+++ adpharma/adpharma.server/src/main/java/org/adorsys/adpharma/server/rest/ProcurementOrderEJB.java
@@ -34,172 +34,174 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class ProcurementOrderEJB
 {
 
-   @Inject
-   private ProcurementOrderRepository repository;
+	@Inject
+	private ProcurementOrderRepository repository;
 
-   @Inject
-   private LoginMerger loginMerger;
+	@Inject
+	private LoginMerger loginMerger;
 
-   @Inject
-   private ProcurementOrderItemMerger procurementOrderItemMerger;
+	@Inject
+	private ProcurementOrderItemMerger procurementOrderItemMerger;
 
-   @Inject
-   private SupplierMerger supplierMerger;
+	@Inject
+	private SupplierMerger supplierMerger;
 
-   @Inject
-   private VATMerger vATMerger;
+	@Inject
+	private VATMerger vATMerger;
 
-   @Inject
-   private AgencyMerger agencyMerger;
-   
-   @Inject
-   private ProcurementOrderItemEJB procurementOrderItemEJB;
+	@Inject
+	private AgencyMerger agencyMerger;
+
+	@Inject
+	private ProcurementOrderItemEJB procurementOrderItemEJB;
 
 	@EJB
 	private DeliveryItemEJB deliveryItemEJB;
 	@EJB
 	private SecurityUtil securityUtil;
-   
-   public ProcurementOrder create(ProcurementOrder entity)
-   {
-      return repository.save(attach(entity));
-   }
 
-   public ProcurementOrder deleteById(Long id)
-   {
-      ProcurementOrder entity = repository.findBy(id);
-      if (entity != null)
-      {
-         repository.remove(entity);
-      }
-      return entity;
-   }
+	public ProcurementOrder create(ProcurementOrder entity)
+	{
+		return repository.save(attach(entity));
+	}
 
-   public ProcurementOrder update(ProcurementOrder entity)
-   {
-      return repository.save(attach(entity));
-   }
+	public ProcurementOrder deleteById(Long id)
+	{
+		ProcurementOrder entity = repository.findBy(id);
+		if (entity != null)
+		{
+			repository.remove(entity);
+		}
+		return entity;
+	}
 
-   public ProcurementOrder findById(Long id)
-   {
-      return repository.findBy(id);
-   }
+	public ProcurementOrder update(ProcurementOrder entity)
+	{
+		return repository.save(attach(entity));
+	}
 
-   public List<ProcurementOrder> listAll(int start, int max)
-   {
-      return repository.findAll(start, max);
-   }
+	public ProcurementOrder findById(Long id)
+	{
+		return repository.findBy(id);
+	}
 
-   public Long count()
-   {
-      return repository.count();
-   }
+	public List<ProcurementOrder> listAll(int start, int max)
+	{
+		return repository.findAll(start, max);
+	}
 
-   public List<ProcurementOrder> findBy(ProcurementOrder entity, int start, int max, SingularAttribute<ProcurementOrder, ?>[] attributes)
-   {
-      return repository.findBy(entity, start, max, attributes);
-   }
+	public Long count()
+	{
+		return repository.count();
+	}
 
-   public Long countBy(ProcurementOrder entity, SingularAttribute<ProcurementOrder, ?>[] attributes)
-   {
-      return repository.count(entity, attributes);
-   }
+	public List<ProcurementOrder> findBy(ProcurementOrder entity, int start, int max, SingularAttribute<ProcurementOrder, ?>[] attributes)
+	{
+		return repository.findBy(entity, start, max, attributes);
+	}
 
-   public List<ProcurementOrder> findByLike(ProcurementOrder entity, int start, int max, SingularAttribute<ProcurementOrder, ?>[] attributes)
-   {
-      return repository.findByLike(entity, start, max, attributes);
-   }
+	public Long countBy(ProcurementOrder entity, SingularAttribute<ProcurementOrder, ?>[] attributes)
+	{
+		return repository.count(entity, attributes);
+	}
 
-   public Long countByLike(ProcurementOrder entity, SingularAttribute<ProcurementOrder, ?>[] attributes)
-   {
-      return repository.countLike(entity, attributes);
-   }
+	public List<ProcurementOrder> findByLike(ProcurementOrder entity, int start, int max, SingularAttribute<ProcurementOrder, ?>[] attributes)
+	{
+		return repository.findByLike(entity, start, max, attributes);
+	}
 
-   private ProcurementOrder attach(ProcurementOrder entity)
-   {
-      if (entity == null)
-         return null;
+	public Long countByLike(ProcurementOrder entity, SingularAttribute<ProcurementOrder, ?>[] attributes)
+	{
+		return repository.countLike(entity, attributes);
+	}
 
-      // aggregated
-      entity.setCreatingUser(loginMerger.bindAggregated(entity.getCreatingUser()));
+	private ProcurementOrder attach(ProcurementOrder entity)
+	{
+		if (entity == null)
+			return null;
 
-      // aggregated
-      entity.setSupplier(supplierMerger.bindAggregated(entity.getSupplier()));
+		// aggregated
+		entity.setCreatingUser(loginMerger.bindAggregated(entity.getCreatingUser()));
 
-      // aggregated
-      entity.setAgency(agencyMerger.bindAggregated(entity.getAgency()));
+		// aggregated
+		entity.setSupplier(supplierMerger.bindAggregated(entity.getSupplier()));
 
-      // aggregated
-      entity.setVat(vATMerger.bindAggregated(entity.getVat()));
+		// aggregated
+		entity.setAgency(agencyMerger.bindAggregated(entity.getAgency()));
 
-      // composed collections
-      Set<ProcurementOrderItem> procurementOrderItems = entity.getProcurementOrderItems();
-      for (ProcurementOrderItem procurementOrderItem : procurementOrderItems)
-      {
-         procurementOrderItem.setProcurementOrder(entity);
-      }
+		// aggregated
+		entity.setVat(vATMerger.bindAggregated(entity.getVat()));
 
-      return entity;
-   }
-   
-   public void handleStockMovementEvent(@Observes @DocumentProcessedEvent StockMovement stockMovement){
-		Article article = stockMovement.getArticle();
-		ProcurementOrderItem searchEntity = new ProcurementOrderItem();
-		searchEntity.setArticle(article);
-		DeliveryItem deliveryItem = new DeliveryItem();
-		deliveryItem.setInternalPic(stockMovement.getInternalPic());
-		deliveryItem.setArticle(stockMovement.getArticle());
-		List<DeliveryItem> foundDI = deliveryItemEJB.findBy(deliveryItem, 0, 1, new SingularAttribute[]{DeliveryItem_.internalPic, DeliveryItem_.article});
-		if(foundDI==null) throw new IllegalStateException("Deliveryitem associated with this stock movement not found.");
-		deliveryItem = foundDI.iterator().next();
-		List<ProcurementOrderItem> found = procurementOrderItemEJB.findBy(searchEntity, 0, 1, new SingularAttribute[]{ProcurementOrderItem_.article, ProcurementOrderItem_.poStatus});
-		ProcurementOrderItem procurementOrderItem = null;
-		if(found.isEmpty()){
-			Login user = securityUtil.getConnectedUser();
-			Agency agency = user.getAgency();
-			procurementOrderItem = new ProcurementOrderItem();
-			procurementOrderItem.setArticle(article);
-			procurementOrderItem.setArticleName(deliveryItem.getArticleName());
-			procurementOrderItem.setCreatingUser(user);
-			procurementOrderItem.setMainPic(article.getPic());
-			procurementOrderItem.setPoStatus(DocumentProcessingState.ONGOING);
-			procurementOrderItem.setProductRecCreated(new Date());
-			procurementOrderItem.setPurchasePricePU(deliveryItem.getPurchasePricePU());
-			procurementOrderItem.setQtyOrdered(BigDecimal.ZERO);
-			procurementOrderItem.setSalesPricePU(deliveryItem.getSalesPricePU());
-			procurementOrderItem.setSecondaryPic(deliveryItem.getSecondaryPic());
-			procurementOrderItem.setStockQuantity(article.getQtyInStock());
-			procurementOrderItem.setValid(Boolean.FALSE);
-			ProcurementOrder procurementOrder = new ProcurementOrder();
-			procurementOrder.setPoStatus(DocumentProcessingState.ONGOING);
-			Supplier supplier = deliveryItem.getDelivery().getSupplier();
-			procurementOrder.setSupplier(supplier);
-			List<ProcurementOrder> foundPOs = findBy(procurementOrder, 0, 1, new SingularAttribute[]{ProcurementOrder_.poStatus,ProcurementOrder_.supplier});
-			if(!foundPOs.isEmpty()){
-				procurementOrder = foundPOs.iterator().next();
-			} else {
-				procurementOrder = new ProcurementOrder();
-				procurementOrder.setAgency(agency);
-				procurementOrder.setCreatedDate(new Date());
-				procurementOrder.setCreatingUser(user);
+		// composed collections
+		Set<ProcurementOrderItem> procurementOrderItems = entity.getProcurementOrderItems();
+		for (ProcurementOrderItem procurementOrderItem : procurementOrderItems)
+		{
+			procurementOrderItem.setProcurementOrder(entity);
+		}
+
+		return entity;
+	}
+
+	public void handleStockMovementEvent(@Observes @DocumentProcessedEvent StockMovement stockMovement){
+		if(stockMovement.canEnablePurchase()){
+			Article article = stockMovement.getArticle();
+			ProcurementOrderItem searchEntity = new ProcurementOrderItem();
+			searchEntity.setArticle(article);
+			DeliveryItem deliveryItem = new DeliveryItem();
+			deliveryItem.setInternalPic(stockMovement.getInternalPic());
+			deliveryItem.setArticle(stockMovement.getArticle());
+			List<DeliveryItem> foundDI = deliveryItemEJB.findBy(deliveryItem, 0, 1, new SingularAttribute[]{DeliveryItem_.internalPic, DeliveryItem_.article});
+			if(foundDI==null) throw new IllegalStateException("Deliveryitem associated with this stock movement not found.");
+			deliveryItem = foundDI.iterator().next();
+			List<ProcurementOrderItem> found = procurementOrderItemEJB.findBy(searchEntity, 0, 1, new SingularAttribute[]{ProcurementOrderItem_.article, ProcurementOrderItem_.poStatus});
+			ProcurementOrderItem procurementOrderItem = null;
+			if(found.isEmpty()){
+				Login user = securityUtil.getConnectedUser();
+				Agency agency = user.getAgency();
+				procurementOrderItem = new ProcurementOrderItem();
+				procurementOrderItem.setArticle(article);
+				procurementOrderItem.setArticleName(deliveryItem.getArticleName());
+				procurementOrderItem.setCreatingUser(user);
+				procurementOrderItem.setMainPic(article.getPic());
+				procurementOrderItem.setPoStatus(DocumentProcessingState.ONGOING);
+				procurementOrderItem.setProductRecCreated(new Date());
+				procurementOrderItem.setPurchasePricePU(deliveryItem.getPurchasePricePU());
+				procurementOrderItem.setQtyOrdered(BigDecimal.ZERO);
+				procurementOrderItem.setSalesPricePU(deliveryItem.getSalesPricePU());
+				procurementOrderItem.setSecondaryPic(deliveryItem.getSecondaryPic());
+				procurementOrderItem.setStockQuantity(article.getQtyInStock());
+				procurementOrderItem.setValid(Boolean.FALSE);
+				ProcurementOrder procurementOrder = new ProcurementOrder();
 				procurementOrder.setPoStatus(DocumentProcessingState.ONGOING);
-				procurementOrder.setProcurementOrderNumber("PO-"+RandomStringUtils.randomAlphanumeric(5).toUpperCase());
-				procurementOrder.setProcurementOrderType(ProcurementOrderType.ORDINARY);
+				Supplier supplier = deliveryItem.getDelivery().getSupplier();
 				procurementOrder.setSupplier(supplier);
-				procurementOrder = create(procurementOrder);
+				List<ProcurementOrder> foundPOs = findBy(procurementOrder, 0, 1, new SingularAttribute[]{ProcurementOrder_.poStatus,ProcurementOrder_.supplier});
+				if(!foundPOs.isEmpty()){
+					procurementOrder = foundPOs.iterator().next();
+				} else {
+					procurementOrder = new ProcurementOrder();
+					procurementOrder.setAgency(agency);
+					procurementOrder.setCreatedDate(new Date());
+					procurementOrder.setCreatingUser(user);
+					procurementOrder.setPoStatus(DocumentProcessingState.ONGOING);
+					procurementOrder.setProcurementOrderNumber("PO-"+RandomStringUtils.randomAlphanumeric(5).toUpperCase());
+					procurementOrder.setProcurementOrderType(ProcurementOrderType.ORDINARY);
+					procurementOrder.setSupplier(supplier);
+					procurementOrder = create(procurementOrder);
+				}
+				procurementOrderItem.setProcurementOrder(procurementOrder);
+				procurementOrderItem = procurementOrderItemEJB.create(procurementOrderItem);
+			} else {
+				procurementOrderItem = found.iterator().next();
 			}
-			procurementOrderItem.setProcurementOrder(procurementOrder);
-			procurementOrderItem = procurementOrderItemEJB.create(procurementOrderItem);
-		} else {
-			procurementOrderItem = found.iterator().next();
+
+			if(stockMovement.isReturnStockMovement()){
+				procurementOrderItem.setQtyOrdered(procurementOrderItem.getQtyOrdered().subtract(stockMovement.getMovedQty()));
+			}else {
+				procurementOrderItem.setQtyOrdered(procurementOrderItem.getQtyOrdered().add(stockMovement.getMovedQty()));
+			}
+			procurementOrderItem.setTotalPurchasePrice(procurementOrderItem.getQtyOrdered().multiply(procurementOrderItem.getPurchasePricePU()));
+			procurementOrderItem = procurementOrderItemEJB.update(procurementOrderItem);
 		}
-		
-		if(StockMovementType.IN.equals(stockMovement.getMovementType())){
-			procurementOrderItem.setQtyOrdered(procurementOrderItem.getQtyOrdered().subtract(stockMovement.getMovedQty()));
-		} else {
-			procurementOrderItem.setQtyOrdered(procurementOrderItem.getQtyOrdered().add(stockMovement.getMovedQty()));
-		}
-		procurementOrderItem.setTotalPurchasePrice(procurementOrderItem.getQtyOrdered().multiply(procurementOrderItem.getPurchasePricePU()));
-		procurementOrderItem = procurementOrderItemEJB.update(procurementOrderItem);
 	}
 }
