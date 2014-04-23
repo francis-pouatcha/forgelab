1293a5b8bd19584b2c2c4ae9f61cae534a6d6992
gakam clovis <clovis.gakam@gmail.com>
2014-04-23 12:06:18 +0100

    add custom list view for procurement order

--- adpharma/adpharma.client/src/main/java/org/adorsys/adpharma/client/jpa/procurementorder/ProcurementOrderController.java
+++ adpharma/adpharma.client/src/main/java/org/adorsys/adpharma/client/jpa/procurementorder/ProcurementOrderController.java
@@ -27,164 +27,164 @@ import org.adorsys.adpharma.client.jpa.procurementorder.ProcurementOrder;
 public class ProcurementOrderController extends DomainComponentController
 {
 
-   @Inject
-   private ProcurementOrderSearchController searchController;
+	@Inject
+	private ProcurementOrderSearchController searchController;
 
-   @Inject
-   private ProcurementOrderCreateController createController;
+	@Inject
+	private ProcurementOrderCreateController createController;
 
-   @Inject
-   private ProcurementOrderEditController editController;
+	@Inject
+	private ProcurementOrderEditController editController;
 
-   @Inject
-   private ProcurementOrderListController listController;
+	@Inject
+	private ProcurementOrderListController listController;
 
-   @Inject
-   private ProcurementOrderDisplayController displayController;
+	@Inject
+	private ProcurementOrderDisplayController displayController;
 
-   @Inject
-   private ProcurementOrderIntialScreenController intialScreenController;
+	@Inject
+	private ProcurementOrderIntialScreenController intialScreenController;
 
-   @Inject
-   @CreateModelEvent
-   private Event<ProcurementOrder> createModelEvent;
-   @Inject
-   @SearchModelEvent
-   private Event<ProcurementOrder> searchModelEvent;
-   @Inject
-   @SelectedModelEvent
-   private Event<ProcurementOrder> selectedModelEvent;
+	@Inject
+	@CreateModelEvent
+	private Event<ProcurementOrder> createModelEvent;
+	@Inject
+	@SearchModelEvent
+	private Event<ProcurementOrder> searchModelEvent;
+	@Inject
+	@SelectedModelEvent
+	private Event<ProcurementOrder> selectedModelEvent;
 
-   private ProcurementOrder createModel = new ProcurementOrder();
-   private ProcurementOrder searchModel = new ProcurementOrder();
-   private ProcurementOrder selectedModel = new ProcurementOrder();
+	private ProcurementOrder createModel = new ProcurementOrder();
+	private ProcurementOrder searchModel = new ProcurementOrder();
+	private ProcurementOrder selectedModel = new ProcurementOrder();
 
-   @Inject
-   private ProcurementOrderRegistration registration;
+	@Inject
+	private ProcurementOrderRegistration registration;
 
-   @Override
-   protected void initViews(Map<ViewType, EntityController> entityViews)
-   {
-      entityViews.put(searchController.getViewType(), searchController);
-      entityViews.put(listController.getViewType(), listController);
-      entityViews.put(displayController.getViewType(), displayController);
-      entityViews.put(editController.getViewType(), editController);
-      entityViews.put(createController.getViewType(), createController);
-      createModelEvent.fire(createModel);
-      searchModelEvent.fire(searchModel);
-      selectedModelEvent.fire(selectedModel);
-   }
+	@Override
+	protected void initViews(Map<ViewType, EntityController> entityViews)
+	{
+		entityViews.put(searchController.getViewType(), searchController);
+		entityViews.put(listController.getViewType(), listController);
+		entityViews.put(displayController.getViewType(), displayController);
+		entityViews.put(editController.getViewType(), editController);
+		entityViews.put(createController.getViewType(), createController);
+		createModelEvent.fire(createModel);
+		searchModelEvent.fire(searchModel);
+		selectedModelEvent.fire(selectedModel);
+	}
 
-   /**
-    * Listen to search result and display.
-    * @param entities
-    */
-   public void handleSearchResult(@Observes @EntitySearchDoneEvent List<ProcurementOrder> entities)
-   {
-      if (!registration.canRead())
-         return;
+	/**
+	 * Listen to search result and display.
+	 * @param entities
+	 */
+	 public void handleSearchResult(@Observes @EntitySearchDoneEvent List<ProcurementOrder> entities)
+	 {
+		 if (!registration.canRead())
+			 return;
 
-      // if result is empty: display no result.
-      if (!getDisplayedViews().contains(listController))
-      {
-         getDisplayedViews().add(listController);
-      }
+		 // if result is empty: display no result.
+		 if (!getDisplayedViews().contains(listController))
+		 {
+			 getDisplayedViews().add(listController);
+		 }
 
-      displayComponent();
-   }
+		 displayComponent();
+	 }
 
-   /**
-    * Listens to list selection events and display
-    */
-   public void handleSelectionEvent(@Observes @EntitySelectionEvent ProcurementOrder selectedEntity)
-   {
-      if (!registration.canRead())
-         return;
-      // if result is empty: display no result.
-      // else display list of procurementOrders.
-      List<EntityController> displayedViews = getDisplayedViews();
-      displayedViews.clear();
-      displayedViews.add(listController);
-      displayedViews.add(displayController);
+	 /**
+	  * Listens to list selection events and display
+	  */
+	 public void handleSelectionEvent(@Observes @EntitySelectionEvent ProcurementOrder selectedEntity)
+	 {
+		 if (!registration.canRead())
+			 return;
+		 // if result is empty: display no result.
+		 // else display list of procurementOrders.
+		 List<EntityController> displayedViews = getDisplayedViews();
+		 displayedViews.clear();
+		 //      displayedViews.add(listController);
+		 displayedViews.add(displayController);
 
-      displayComponent();
-   }
+		 displayComponent();
+	 }
 
-   /**
-    * Display the search and list panel
-    * @param selectedEntity
-    */
-   public void handleSearchRequestedEvent(@Observes @EntitySearchRequestedEvent ProcurementOrder selectedEntity)
-   {
-      if (!registration.canRead())
-         return;
-      List<EntityController> displayedViews = getDisplayedViews();
-      displayedViews.clear();
-      displayedViews.add(searchController);
-      displayedViews.add(listController);
-      displayComponent();
-   }
+	 /**
+	  * Display the search and list panel
+	  * @param selectedEntity
+	  */
+	 public void handleSearchRequestedEvent(@Observes @EntitySearchRequestedEvent ProcurementOrder selectedEntity)
+	 {
+		 if (!registration.canRead())
+			 return;
+		 List<EntityController> displayedViews = getDisplayedViews();
+		 displayedViews.clear();
+		 //      displayedViews.add(searchController);
+		 displayedViews.add(listController);
+		 displayComponent();
+	 }
 
-   /**
-    * Display search form.
-    * @param selectedProcurementOrder
-    */
-   public void handleCreateRequestedEvent(@Observes @EntityCreateRequestedEvent ProcurementOrder templateEntity)
-   {
-      if (!registration.canCreate())
-         return;
-      List<EntityController> displayedViews = getDisplayedViews();
-      displayedViews.clear();
-      displayedViews.add(listController);
-      displayedViews.add(createController);
-      displayComponent();
-   }
+	 /**
+	  * Display search form.
+	  * @param selectedProcurementOrder
+	  */
+	 public void handleCreateRequestedEvent(@Observes @EntityCreateRequestedEvent ProcurementOrder templateEntity)
+	 {
+		 if (!registration.canCreate())
+			 return;
+		 List<EntityController> displayedViews = getDisplayedViews();
+		 displayedViews.clear();
+		 //      displayedViews.add(listController);
+		 displayedViews.add(createController);
+		 displayComponent();
+	 }
 
-   public void handleEditRequestedEvent(@Observes @EntityEditRequestedEvent ProcurementOrder selectedEntity)
-   {
-      if (!registration.canEdit())
-         return;
-      List<EntityController> displayedViews = getDisplayedViews();
-      displayedViews.clear();
-      displayedViews.add(listController);
-      displayedViews.add(editController);
-      displayComponent();
-   }
+	 public void handleEditRequestedEvent(@Observes @EntityEditRequestedEvent ProcurementOrder selectedEntity)
+	 {
+		 if (!registration.canEdit())
+			 return;
+		 List<EntityController> displayedViews = getDisplayedViews();
+		 displayedViews.clear();
+		 //      displayedViews.add(listController);
+		 displayedViews.add(editController);
+		 displayComponent();
+	 }
 
-   public void handleEditCanceledEvent(@Observes @EntityEditCanceledEvent ProcurementOrder selectedEntity)
-   {
-      List<EntityController> displayedViews = getDisplayedViews();
-      displayedViews.clear();
-      displayedViews.add(listController);
-      displayedViews.add(displayController);
+	 public void handleEditCanceledEvent(@Observes @EntityEditCanceledEvent ProcurementOrder selectedEntity)
+	 {
+		 List<EntityController> displayedViews = getDisplayedViews();
+		 displayedViews.clear();
+		 displayedViews.add(listController);
+		 //      displayedViews.add(displayController);
 
-      displayComponent();
-   }
+		 displayComponent();
+	 }
 
-   public void handleEditDoneEvent(@Observes @EntityEditDoneEvent ProcurementOrder selectedEntity)
-   {
-      List<EntityController> displayedViews = getDisplayedViews();
-      displayedViews.clear();
-      displayedViews.add(listController);
-      displayedViews.add(displayController);
+	 public void handleEditDoneEvent(@Observes @EntityEditDoneEvent ProcurementOrder selectedEntity)
+	 {
+		 List<EntityController> displayedViews = getDisplayedViews();
+		 displayedViews.clear();
+		 displayedViews.add(listController);
+		 //      displayedViews.add(displayController);
 
-      displayComponent();
-   }
+		 displayComponent();
+	 }
 
-   @Override
-   protected void selectDisplay()
-   {
-      if (searchController != null)
-      {
-         intialScreenController.startWithSeach();
-      }
-      else if (createController != null)
-      {
-         intialScreenController.startWithCreate();
-      }
-      else
-      {
-         throw new IllegalStateException("Missing search and display component.");
-      }
-   }
+	 @Override
+	 protected void selectDisplay()
+	 {
+		 if (searchController != null)
+		 {
+			 intialScreenController.startWithSeach();
+		 }
+		 else if (createController != null)
+		 {
+			 intialScreenController.startWithCreate();
+		 }
+		 else
+		 {
+			 throw new IllegalStateException("Missing search and display component.");
+		 }
+	 }
 }
