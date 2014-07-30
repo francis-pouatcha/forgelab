7d9efe7426eac58eb1b7afe34f128b004f0679ad
francis-pouatcha <francis.pouatcha@adorsys.com>
2014-07-30 02:59:44 -0400

    correct interfacing of form authentication

--- adpharma/adpharma.server/src/main/webapp/js/services/httpInterceptor.js
+++ adpharma/adpharma.server/src/main/webapp/js/services/httpInterceptor.js
@@ -7,11 +7,16 @@ angular.module('adpharma').factory('httpInterceptor', function httpInterceptor (
       };
 
       var error = function (response) {
-          if (response.status === 401) {
-              $location.url('/');
+    	  // Adpharma server react on 403 forbiden and not on 401. We can keep both.
+          if (response.status === 401 || response.status === 403) {
+        	  // Forward to login page and return response after setting the location, if not
+        	  // the application will display an error.
+              $location.url('/login');
+              return response;
+          } else {
+        	  return $q.reject(response);
           }
 
-          return $q.reject(response);
       };
 
       return promise.then(success, error);
