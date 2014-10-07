cbfe12b492b40c8f176bc59beaa70aa577506c5b
hsimofranklin <franklin.hsimo@afrotools.com>
2014-10-07 10:49:25 +0100

    Test receipt height

--- adpharma/adpharma.client/src/main/java/org/adorsys/adpharma/client/jpa/customerinvoice/ReceiptDataTable.java
+++ adpharma/adpharma.client/src/main/java/org/adorsys/adpharma/client/jpa/customerinvoice/ReceiptDataTable.java
@@ -28,7 +28,7 @@ public class ReceiptDataTable {
 		for (ReceiptDataRow receiptDataRow : receiptDataRows) {
 			height+=receiptDataRow.getHeight();
 		}
-		return height + tableMetaData.getRowHeight() + 2 * (tableMetaData.getMargin());
+		return height + 2 * tableMetaData.getRowHeight() + 2 * (tableMetaData.getMargin());
 	}
 
 	public ReceiptDataTableMetaData getTableMetaData() {
