9cfcfc6c73a2190a8f683e9b4423befaaf92d5cd
gakam clovis <clovis.gakam@gmail.com>
2014-11-05 11:05:01 +0100

    mersge

--- adpharma/adpharma.server/src/main/java/org/adorsys/adpharma/server/repo/SalesOrderRepository.java
+++ adpharma/adpharma.server/src/main/java/org/adorsys/adpharma/server/repo/SalesOrderRepository.java
@@ -3,6 +3,7 @@ package org.adorsys.adpharma.server.repo;
 import java.math.BigDecimal;
 
 import org.adorsys.adpharma.server.jpa.CashDrawer;
+import org.adorsys.adpharma.server.jpa.DocumentProcessingState;
 import org.adorsys.adpharma.server.jpa.SalesOrder;
 import org.apache.deltaspike.data.api.Query;
 import org.apache.deltaspike.data.api.Repository;
@@ -11,7 +12,7 @@ import org.apache.deltaspike.data.api.Repository;
 public abstract class SalesOrderRepository extends AdpharmaEntityRepository<SalesOrder, Long>
 {
 
-	@Query("SELECT SUM((s.amountAfterTax * s.insurance.coverageRate)/100) FROM SalesOrder AS s WHERE s.insurance <> NULL AND s.cashDrawer = ?1 ")
+	@Query("SELECT SUM((s.amountAfterTax * s.insurance.coverageRate)/100) FROM SalesOrder AS s WHERE s.insurance <> NULL AND s.cashDrawer = ?1 AND s.cashed = true  ")
 	public abstract BigDecimal getInsurranceSalesByCashDrawer(CashDrawer cashDrawer) ;
 	
 	@Query("SELECT SUM(s.amountDiscount) FROM SalesOrder AS s WHERE s.cashDrawer = ?1 AND s.cashed = ?2 ")
